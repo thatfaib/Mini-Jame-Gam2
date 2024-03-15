46cefaabc7fd923e13b176b19307ce3632b8f402
thatfaib <r.z.swag00@gmail.com>
2024-03-15 14:58:31 +0100

    amogus

--- Mini jame gam2/Assets/NewBehaviourScript.cs
+++ Mini jame gam2/Assets/NewBehaviourScript.cs
@@ -7,12 +7,6 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Amogus")
     }
 }
